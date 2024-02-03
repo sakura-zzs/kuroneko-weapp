c09254db104629b05f777c21b2eca8e621b88349
sakura-zzs <sakura20201029@126.com>
2024-02-03 21:41:50 +0800

    init:项目初始化 tabbar初步封装

---
+++ miniprogram/components/navigation-bar/navigation-bar.ts
@@ -0,0 +1,27 @@
+// components/navigation-bar.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:"默认标题"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
